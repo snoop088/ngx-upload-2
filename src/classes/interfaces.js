1568514c96d3dd74413e96348ba1b855fcbc7d57
Nikolay Dimitrov <snoop088@gmail.com>
2017-11-24 22:10:21 +0200

    ADD post:build to clean js and json files from src

--- src/classes/interfaces.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var UploadStatus;
-(function (UploadStatus) {
-    UploadStatus[UploadStatus["Queue"] = 0] = "Queue";
-    UploadStatus[UploadStatus["Uploading"] = 1] = "Uploading";
-    UploadStatus[UploadStatus["Done"] = 2] = "Done";
-    UploadStatus[UploadStatus["Cancelled"] = 3] = "Cancelled";
-})(UploadStatus = exports.UploadStatus || (exports.UploadStatus = {}));
